93876c5f8363f2d09a4d8f6dcc62b1f86d487297
Vikrant Toppo <143046393+vt24vikrant@users.noreply.github.com>
2024-09-16 05:36:08 +0530

    Update calculator.cpp
    1. Added divisibility by zero check
    2. Changed if (value='a') to if (value == 'a') and similarly for value == 'b'
    3. Added space for better readability in the results.

--- C++/calculator.cpp
+++ C++/calculator.cpp
@@ -1,117 +1,119 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+void single() {
+    unsigned int a, b;
+    char m;
+    cout << "Enter the type of process that you want to do (+, -, *, /): ";
+    cin >> m;
 
-void single(){
- unsigned int a,b;
-   char m;
-cout<<"Enter the type of process that you wana do(+,-,*,/) :";
-cin>>m;
-
-    switch(m){
-        case '+':{
-            cout<<"Enter the first number :";
-            cin>>a;
-             cout<<"Enter the second number :";
-            cin>>b;
-
-            cout<<"The sum is :"<<a+b;
+    switch (m) {
+        case '+': {
+            cout << "Enter the first number: ";
+            cin >> a;
+            cout << "Enter the second number: ";
+            cin >> b;
+            cout << "The result is: " << a + b << endl;
             break;
         }
-         case '*':{
-            cout<<"Enter the first number :";
-            cin>>a;
-             cout<<"Enter the second number :";
-            cin>>b;
-
-            cout<<"The sum is :"<<a*b;
+        case '*': {
+            cout << "Enter the first number: ";
+            cin >> a;
+            cout << "Enter the second number: ";
+            cin >> b;
+            cout << "The result is: " << a * b << endl;
             break;
         }
-         case '-':{
-            cout<<"Enter the first number :";
-            cin>>a;
-             cout<<"Enter the second number :";
-            cin>>b;
-
-            cout<<"The sum is :"<<a-b;
+        case '-': {
+            cout << "Enter the first number: ";
+            cin >> a;
+            cout << "Enter the second number: ";
+            cin >> b;
+            cout << "The result is: " << a - b << endl;
             break;
         }
-         case '/':{
-            cout<<"Enter the first number :";
-            cin>>a;
-             cout<<"Enter the second number :";
-            cin>>b;
-
-            cout<<"The sum is :"<<a/b;
+        case '/': {
+            cout << "Enter the first number: ";
+            cin >> a;
+            cout << "Enter the second number: ";
+            cin >> b;
+            if (b == 0)
+                cout << "Error: Division by zero is undefined." << endl;
+            else
+                cout << "The result is: " << a / b << endl;
             break;
         }
-
-         default :{
-            cout<<"invalid";
+        default: {
+            cout << "Invalid operation." << endl;
+            break;
         }
     }
 }
 
-void multi(){
-  float a,b;
-   char m;
-cout<<"Enter the type of process that you wana do(+,-,*,/) :";
-cin>>m;
+void multi() {
+    float a, b;
+    char m;
+    cout << "Enter the type of process that you want to do (+, -, *, /): ";
+    cin >> m;
 
-    switch(m){
-        case '+':{
-            cout<<"Enter the first number :";
-            cin>>a;
-             cout<<"Enter the second number :";
-            cin>>b;
-
-            cout<<"The sum is :"<<a+b;
+    switch (m) {
+        case '+': {
+            cout << "Enter the first number: ";
+            cin >> a;
+            cout << "Enter the second number: ";
+            cin >> b;
+            cout << "The result is: " << a + b << endl;
             break;
         }
-         case '*':{
-            cout<<"Enter the first number :";
-            cin>>a;
-             cout<<"Enter the second number :";
-            cin>>b;
-
-            cout<<"The sum is :"<<a*b;
+        case '*': {
+            cout << "Enter the first number: ";
+            cin >> a;
+            cout << "Enter the second number: ";
+            cin >> b;
+            cout << "The result is: " << a * b << endl;
             break;
         }
-         case '-':{
-            cout<<"Enter the first number :";
-            cin>>a;
-             cout<<"Enter the second number :";
-            cin>>b;
-
-            cout<<"The sum is :"<<a-b;
+        case '-': {
+            cout << "Enter the first number: ";
+            cin >> a;
+            cout << "Enter the second number: ";
+            cin >> b;
+            cout << "The result is: " << a - b << endl;
             break;
         }
-         case '/':{
-            cout<<"Enter the first number :";
-            cin>>a;
-             cout<<"Enter the second number :";
-            cin>>b;
-
-            cout<<"The sum is :"<<a/b;
+        case '/': {
+            cout << "Enter the first number: ";
+            cin >> a;
+            cout << "Enter the second number: ";
+            cin >> b;
+            if (b == 0)
+                cout << "Error: Division by zero is undefined." << endl;
+            else
+                cout << "The result is: " << a / b << endl;
             break;
         }
-
-         default :{
-            cout<<"invalid";
+        default: {
+            cout << "Invalid operation." << endl;
+            break;
         }
     }
 }
 
-int main(){
+int main() {
     char value;
-   cout<<"------------WELCOME!!!!------------"<<"\n";
-   cout<<"What type of process would you wana do?"<<"\n"<<"1)Press (a) for non-decimal "<<"\n"<<"2)Press (b) for decimal operation(Can be done for positive value only)"<<"\n";
-   cin>>value;
-   if(value='a'){
-    single();
-   }else if(value='b'){
-    multi();
-   }else{
-    cout<<"You have entered a wrong value.";
-   }
+    cout << "------------WELCOME!!!!------------" << endl;
+    cout << "What type of process would you like to do?" << endl;
+    cout << "1) Press (a) for non-decimal operations" << endl;
+    cout << "2) Press (b) for decimal operations (can be done for positive values only)" << endl;
+    cin >> value;
+
+    if (value == 'a') {
+        single();
+    } else if (value == 'b') {
+        multi();
+    } else {
+        cout << "You have entered a wrong value." << endl;
+    }
+
+    return 0;
 }
